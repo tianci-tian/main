55dfb9078109e79de21049766d320ecf1692a5d9
tianci tian <tianci.tian@linksfield.net>
2024-07-24 17:48:01 +0800

    add Run jar

---
+++ PycharmProjects/离线部署并发/RunJAVAJar.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def RunJAVAJar(jar_path):
+    # 构建命令
+    command = ["C:\\Program Files (x86)\\Common Files\\Oracle\\Java\\javapath\\java.exe", "-jar", jar_path]
+
+    # 启动子进程
+    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
+        output, error = process.communicate()
+    return output
+
