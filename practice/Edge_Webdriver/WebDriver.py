d3717ce9513077f29ef2ff7e059afbdeb360efd0
tianci tian <tianci.tian@linksfield.net>
2024-09-13 16:18:03 +0800

    Edge浏览器积分

--- practice/Edge_Webdriver/WebDriver.py
+++ practice/Edge_Webdriver/WebDriver.py
@@ -1,8 +1,8 @@
 import time
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-
-with open('./data.txt', 'r', encoding='utf-8') as f:
+from selenium.webdriver.support.wait import WebDriverWait
+with (open('./data.txt', 'r', encoding='utf-8') as f):
     lines = f.readlines()
     for line in lines:
         # 创建 Edge 浏览器驱动实例
@@ -11,7 +11,9 @@ with open('./data.txt', 'r', encoding='utf-8') as f:
         driver.get("http://www.bing.com")
         time.sleep(5)
         #登录
-        driver.find_element(By.ID, 'id_p').click()
+        ele = driver.find_element(By.ID, 'id_p')
+        WebDriverWait(driver,20).until(lambda x:ele.is_displayed())
+        ele.click()
         driver.find_element(By.ID,'sb_form_q').send_keys(line)
         #driver.find_element(By.ID,'search_icon').click()
         time.sleep(5)
